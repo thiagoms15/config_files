e4183618de988c7308ab9364e8ec2211f95e583a
Thiago Marques <thiagoms.15@gmail.com>
2021-07-30 22:53:20 +0000

    Updated theme

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,11 +6,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch example
-polybar example &
+# Launch polybar
+polybar integrate &
 
 my_laptop_external_monitor=$(xrandr --query | grep 'HDMI-0')
 if [[ $my_laptop_external_monitor = *connected* ]]; then
     polybar top_external &
 fi
+
 echo "Bars launched..."
